2fc9565acb2de90c6f34eba1a13ff9fe68b7660b
Paul Ledieu <pledieu@z4r12p4.42lyon.fr>
2025-02-18 13:09:13 +0100

    pb w 100 philo

--- src/activity.c
+++ src/activity.c
@@ -6,7 +6,7 @@
 /*   By: pledieu <pledieu@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 10:29:04 by pledieu           #+#    #+#             */
-/*   Updated: 2025/02/18 12:45:27 by pledieu          ###   ########lyon.fr   */
+/*   Updated: 2025/02/18 13:00:38 by pledieu          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ void	eat(t_philo *philo)
 	pthread_mutex_t *first_fork;
 	pthread_mutex_t *second_fork;
 
-	// ✅ Déterminer quelle fourchette prendre en premier
 	if (philo->id % 2 == 0)
 	{
 		first_fork = philo->right_fork;
@@ -29,13 +28,21 @@ void	eat(t_philo *philo)
 		second_fork = philo->right_fork;
 	}
 
+	// ✅ Vérifier avant de prendre les fourchettes
+	pthread_mutex_lock(&philo->data->death_lock);
+	if (get_timestamp() - philo->last_meal > philo->data->time_to_die)
+	{
+		pthread_mutex_unlock(&philo->data->death_lock);
+		return;
+	}
+	pthread_mutex_unlock(&philo->data->death_lock);
+
 	pthread_mutex_lock(first_fork);
 	print_status(philo, "has taken a fork");
 
 	pthread_mutex_lock(second_fork);
 	print_status(philo, "has taken a fork");
 
-	// ✅ Mise à jour immédiate de last_meal
 	pthread_mutex_lock(&philo->data->death_lock);
 	philo->last_meal = get_timestamp();
 	pthread_mutex_unlock(&philo->data->death_lock);
@@ -46,8 +53,6 @@ void	eat(t_philo *philo)
 	pthread_mutex_unlock(second_fork);
 	pthread_mutex_unlock(first_fork);
 }
-
-
 void	sleep_and_think(t_philo *philo)
 {
 	print_status(philo, "is sleeping");
