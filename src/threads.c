d475c1c1c74ff1af027307ab15b1807294763f80
Paul Ledieu <pledieu@z4r11p7.42lyon.fr>
2025-02-19 15:13:50 +0100

    no normed but test seems good

--- src/threads.c
+++ src/threads.c
@@ -6,7 +6,7 @@
 /*   By: pledieu <pledieu@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 10:29:36 by pledieu           #+#    #+#             */
-/*   Updated: 2025/02/18 20:58:00 by pledieu          ###   ########lyon.fr   */
+/*   Updated: 2025/02/19 15:07:48 by pledieu          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,59 +44,57 @@ void	*philosopher_routine(void *arg)
 
 
 
-
-
-
-
-int	start_simulation(t_data *data)
+int start_simulation(t_data *data)
 {
-	pthread_t	death_checker;
+    pthread_t death_checker;
 
-	// Création des threads pour les philosophes
-	for (int i = 0; i < data->num_philos; i++)
-	{
-		if (pthread_create(&data->philos[i].thread, NULL, philosopher_routine, &data->philos[i]))
-			return (printf("Thread creation failed\n"), 1);
-	}
-	// Création du thread qui surveille la mort des philosophes
-	if (pthread_create(&death_checker, NULL, monitor_death, data))
-		return (printf("Death monitor thread failed\n"), 1);
+    for (int i = 0; i < data->num_philos; i++)
+    {
+        if (pthread_create(&data->philos[i].thread, NULL, philosopher_routine, &data->philos[i]))
+            return (printf("Thread creation failed\n"), 1);
+        usleep(100); // Petit délai pour éviter la congestion
+    }
 
-	// Attendre la fin des threads
-	for (int i = 0; i < data->num_philos; i++)
-		pthread_join(data->philos[i].thread, NULL);
-	pthread_join(death_checker, NULL);
+    if (pthread_create(&death_checker, NULL, monitor_death, data))
+        return (printf("Death monitor thread failed\n"), 1);
 
-	return (0);
+    for (int i = 0; i < data->num_philos; i++)
+        pthread_join(data->philos[i].thread, NULL);
+    pthread_join(death_checker, NULL);
+
+    return (0);
 }
 
-
-void	*monitor_death(void *arg)
+void *monitor_death(void *arg)
 {
-	t_data	*data = (t_data *)arg;
-	int		i;
+    t_data *data = (t_data *)arg;
+    int i;
 
-	while (data->simulation_running)
-	{
-		i = 0;
-		while (i < data->num_philos)
-		{
-			pthread_mutex_lock(&data->death_lock);
-			if ((get_timestamp() - data->philos[i].last_meal) > data->time_to_die)
-			{
-				pthread_mutex_lock(&data->write_lock);  // 🔒 Bloquer l'affichage
-				printf("%lld %d died\n", get_timestamp() - data->start_time, data->philos[i].id);
-				pthread_mutex_unlock(&data->write_lock);  // 🔓 Débloquer après affichage
+    while (data->simulation_running)
+    {
+        i = 0;
+        while (i < data->num_philos)
+        {
+            pthread_mutex_lock(&data->death_lock);
+            long long time_since_meal = get_timestamp() - data->philos[i].last_meal;
 
-				data->simulation_running = 0;  // 🛑 Arrêter la simulation après affichage
-				pthread_mutex_unlock(&data->death_lock);
-				return (NULL);
-			}
-			pthread_mutex_unlock(&data->death_lock);
-			i++;
-		}
-		usleep(1000);  // Vérifier toutes les 1 ms
-	}
-	return (NULL);
+            if (time_since_meal > data->time_to_die && data->simulation_running)
+            {
+                data->simulation_running = 0;
+                pthread_mutex_unlock(&data->death_lock);
+
+                pthread_mutex_lock(&data->write_lock);
+                printf("%lld %d died\n", get_timestamp() - data->start_time, data->philos[i].id);
+                pthread_mutex_unlock(&data->write_lock);
+
+                return (NULL);
+            }
+            pthread_mutex_unlock(&data->death_lock);
+            i++;
+        }
+        usleep(500);
+    }
+    return (NULL);
 }
+
 
